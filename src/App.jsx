76982433e09b818a3c4b87259d3c16b5b37ded34
ilhardika <ilhardika67@gmail.com>
2025-03-03 22:14:35 +0700

    Refactor App component structure and build Header Seaction and add Search component; remove unused SVG files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <main>
       <div className="pattern"></div>
 
-      <div className="wrapper"></div>
-      <header>
-        <h1>
-          Find <span className="text-gradient">Movies</span> You'll Enjoy Without Hassle
-        </h1>
-      </header>
-      <p>Search</p>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
     </main>
   );
 };
