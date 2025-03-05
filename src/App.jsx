9fa0fbe815c1ac812f9d64a2bd1b48b35b7ba7ee
ilhardika <ilhardika67@gmail.com>
2025-03-05 15:50:05 +0700

    Add Appwrite dependency and implement search count update functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -51,6 +52,7 @@ const App = () => {
         return;
       }
       setMovieList(data.results);
+      updateSearchCount();
     } catch (error) {
       setErrorMessage(`error message : ${error}`);
     } finally {
